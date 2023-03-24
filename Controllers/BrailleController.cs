806bd9d10709ef3f7909fa8d2bdc9c08ffa5c130
Nawam Sahasra Mandara Denawakage <nawamsahasra27th@gmail.com>
2023-03-24 15:40:42 +0530

    Add project files.

---
+++ Controllers/BrailleController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace BrailleWinAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]/api")]
+    public class DotPatterController : ControllerBase
+    {
+        [HttpGet("rectangle/{width}/{height}")]
+        public IActionResult Rectangle(int width, int height)
+        {
+            string dotPattern = "";
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    dotPattern += ". ";
+                }
+                dotPattern += "\n";
+            }
+            return Ok(dotPattern);
+        }
+
+        [HttpGet("circle/{radius}/{resolution}")]
+        public IActionResult GetCircle(int radius, int resolution)
+        {
+            string dotPattern = "";
+            double step = 2 * Math.PI / resolution;
+            for (int y = -radius; y <= radius; y++)
+            {
+                for (int x = -radius; x <= radius; x++)
+                {
+                    double distance = Math.Sqrt(x * x + y * y);
+                    if (Math.Abs(distance - radius) < step / 2)
+                    {
+                        dotPattern += ".";
+                    }
+                    else
+                    {
+                        dotPattern += " ";
+                    }
+                }
+                dotPattern += "\n";
+            }
+            return Ok(dotPattern);
+        }
+    }
+
+    
+}
